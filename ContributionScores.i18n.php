9ad532ae7428177882876b2810d2be5c4cc3aaa9
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-03-31 19:02:21 +0000

    Rename 'tg' to 'tg-cyrl' in extensions

--- ContributionScores.i18n.php
+++ ContributionScores.i18n.php
@@ -669,7 +669,7 @@ $messages['te'] = array(
 /** Tajik (Тоҷикӣ)
  * @author Ibrahim
  */
-$messages['tg'] = array(
+$messages['tg-cyrl'] = array(
 	'contributionscores-pages'    => 'Саҳифаҳо',
 	'contributionscores-changes'  => 'Тағйирот',
 	'contributionscores-username' => 'Номи корбарӣ',
