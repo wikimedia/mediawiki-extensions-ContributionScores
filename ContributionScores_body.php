fe3076c95a22d33a72f8a2996576f58f90993003
Sam Reed <reedy@users.mediawiki.org>
2010-10-29 21:30:20 +0000

    Conditionals in loops to foreachs

--- ContributionScores_body.php
+++ ContributionScores_body.php
@@ -102,7 +102,7 @@ class ContributionScores extends IncludableSpecialPage {
 
 		$skin = $wgUser->getSkin();
 		$altrow = '';
-		while ( $row = $dbr->fetchObject( $res ) ) {
+		foreach ( $res as $row ) {
 			$output .= "</tr><tr class='{$altrow}'>\n<td class='content'>" .
 				round($row->wiki_rank,0) . "\n</td><td class='content'>" .
 				$row->page_count . "\n</td><td class='content'>" .
