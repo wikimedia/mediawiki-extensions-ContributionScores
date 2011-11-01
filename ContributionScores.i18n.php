15186905a63f209011cb70e6b04f402b0df185ce
Raimond Spekking <raymond@users.mediawiki.org>
2011-11-01 21:04:38 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- ContributionScores.i18n.php
+++ ContributionScores.i18n.php
@@ -409,7 +409,7 @@ Zasadnje toś ta licba dypkow měri pśedewšym wobźěłane změny pód źiwani
 pód wuměnjenim až to by było bok z wušeju kwalitu.',
 	'contributionscores-top' => '({{PLURAL:$1|Nejlěpšy $1|Nejlěpšej $1|Nejlěpše $1|Nejlěpšych $1}})',
 	'contributionscores-days' => '{{PLURAL:$1|Slědny $1 źeń|Slědnej $1 dnja|Slědne $1 dny|Slědnych $1 dnjow}}',
-	'contributionscores-allrevisions' => 'Wše wersije',
+	'contributionscores-allrevisions' => 'Ceły cas',
 	'contributionscores-score' => 'Pógódnośenje',
 	'contributionscores-pages' => 'Boki',
 	'contributionscores-changes' => 'Změny',
